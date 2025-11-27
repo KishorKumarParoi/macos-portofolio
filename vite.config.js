ac2578dcb5609eb0be30c0c107e59c0e47b40187
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-11-27 21:56:55 +0600

    Welcome Code

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@components": resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        "components"
-      ),
-      "@constants": resolve(
-        dirname(fileURLToPath(import.meta.url)),
-        "constants"
-      ),
-      "@store": resolve(dirname(fileURLToPath(import.meta.url)), "store"),
-      "@hoc": resolve(dirname(fileURLToPath(import.meta.url)), "hoc"),
-      "@assets": resolve(dirname(fileURLToPath(import.meta.url)), "assets"),
-      "@windows": resolve(dirname(fileURLToPath(import.meta.url)), "windows"),
+      "#/src/components": resolve(__dirname, "src/components"),
+      "#/src/constants": resolve(__dirname, "src/constants"),
+      "#/src/store": resolve(__dirname, "src/store"),
+      "#/src/hoc": resolve(__dirname, "src/hoc"),
+      "#/src/assets": resolve(__dirname, "src/assets"),
+      "#/src/windows": resolve(__dirname, "src/windows"),
+      "#components": resolve(__dirname, "src/components"),
+      "#constants": resolve(__dirname, "src/constants"),
     },
   },
 });
